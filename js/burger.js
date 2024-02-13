a053b6fa11f5f081f55c325e2828bf695c23ee81
Sinsjhonymkp <tohaantoha96@gmail.com>
2024-02-13 18:08:24 +0300

    button up + galeryPage

--- js/burger.js
+++ js/burger.js
@@ -1,16 +1,16 @@
 const burgerButton = document.querySelector(".burger__button");
 const burgerMenu = document.querySelector(".burger__menu");
-const burgerLinks = document.querySelector(".header__container-nav");
+const burgerLinks = document.querySelector("#header__container-nav");
 const burgerLinksItem = document.querySelector(".header__nav-items");
 
-console.log(burgerButton);
-console.log(burgerMenu);
+console.log(burgerLinks);
 burgerButton.addEventListener('click', () =>{
     document.body.style = "overflow: hidden";
     burgerLinks.style = "display: flex";
     burgerLinksItem.style = "flex-direction: column";
  burgerMenu.style = "left: 0%";
 });
+
 burgerMenu.addEventListener('click', () => {
     document.body.style = "overflow: visible";
     burgerMenu.style = "left: -100%";
